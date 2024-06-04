81f5cd8689b5ffe508001b3a14d838059166363d
lucas-bertinchamp <lucas.bertinchamp@depinfonancy.net>
2024-06-03 22:53:22 -0400

    Remove comment

--- viz/viz_template.py
+++ viz/viz_template.py
@@ -3,7 +3,6 @@ import plotly.graph_objects as go
 ## LE TEMPLATE DE FICHIER A UTILISER POUR LES VISUALISATIONS
 ## Chaque fichier de visualisation doit contenir la fonction get_figure(data) qui retourne un objet figure de plotly
 ## Le nom du fichier est : "nom_de_la_visualisation.py"
-## Dans app.py, ajouter dans figures_files le nom du fichier sans l'extension ".py" pour générer la visualisation
 
 def get_figure(data):
     """
