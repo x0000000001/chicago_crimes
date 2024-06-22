43575ae76d6134ad792df58c3a61742d8a6b8a9d
lucas-bertinchamp <lucas.bertinchamp@depinfonancy.net>
2024-06-22 14:44:12 -0400

    Refactor data files

--- viz/stacked_bar_chart.py
+++ viz/stacked_bar_chart.py
@@ -4,6 +4,7 @@ import pandas as pd
 import plotly.graph_objects as go
 from dash import dcc, html
 from dash.dependencies import Input, Output
+from paths import DATA_STACKEDBC_FOLDER
 
 ## LE TEMPLATE DE FICHIER A UTILISER POUR LES VISUALISATIONS
 ## Chaque fichier de visualisation doit contenir la fonction get_figure(data) qui retourne un objet figure de plotly
@@ -11,18 +12,15 @@ from dash.dependencies import Input, Output
 
 
 # Import data
-beat_count = pd.read_csv('beat_count.csv')
-district_count = pd.read_csv('district_count.csv')
-type_count= pd.read_csv('type_count.csv')
+beat_count = pd.read_csv(DATA_STACKEDBC_FOLDER + '/beat_count.csv')
+district_count = pd.read_csv(DATA_STACKEDBC_FOLDER + '/district_count.csv')
+type_count= pd.read_csv(DATA_STACKEDBC_FOLDER + '/type_count.csv')
 
 # Calculate arrest rate
 # J'ai besoin d'une façon de calculer un pourcentage pour true + false pour chaque beat/district/type
 # J'ai aussi besoin d'une façon de choisir entre les 3 data sets (beat/district/type) 
 
-
-
 #creation viz
-
 
 
 def create_stacked_bar(x, y):
@@ -64,7 +62,6 @@ def create_stacked_bar(x, y):
 def get_figure(_data):
     fig = create_stacked_bar("x", "y")
     return fig
-
 
 
 # hover template
